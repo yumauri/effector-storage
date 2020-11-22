dbce5496f01ea0927d1d7b8619122efb955f54de
yumauri <yumaa.verdin@gmail.com>
2020-11-23 00:25:03 +0300

    Add test for localStorage default sync flag

--- tests/local.test.ts
+++ tests/local.test.ts
@@ -10,10 +10,12 @@ import { persist } from '../src/local'
 //
 
 declare let global: any
+let events: ReturnType<typeof createEventsMock>
 
 test.before(() => {
   global.localStorage = createStorageMock()
-  global.addEventListener = createEventsMock().addEventListener
+  events = createEventsMock()
+  global.addEventListener = events.addEventListener
 })
 
 test.after(() => {
@@ -32,6 +34,22 @@ test('should export adapter and `persist` function', () => {
 test('should be ok on good parameters', () => {
   const $store = createStore(0, { name: 'local::store' })
   assert.not.throws(() => persist({ store: $store }))
+})
+
+test('persisted with localStorage store should be synced', async () => {
+  const $counter = createStore(0, { name: 'counter' })
+  persist({ store: $counter })
+  assert.is($counter.getState(), 0)
+
+  global.localStorage.setItem('counter', '1')
+  await events.dispatchEvent('storage', {
+    storageArea: global.localStorage,
+    key: 'counter',
+    oldValue: null,
+    newValue: '1',
+  })
+
+  assert.is($counter.getState(), 1)
 })
 
 //
