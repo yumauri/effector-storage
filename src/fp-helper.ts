7c6a1a41211761556ae738e9c952d4c4af1e34a3
yumauri <yumaa.verdin@gmail.com>
2021-09-28 11:16:04 +0300

    Add deprecation message for `fp`

--- src/fp-helper.ts
+++ src/fp-helper.ts
@@ -4,13 +4,19 @@ type LikePersist = {
   (config: any): Subscription
 }
 
+type Curried = {
+  <State>(store: Store<State>): Store<State>
+}
+
 /**
  * Helper to make any `persist` function like functional
  */
 export const fp =
   <Config>(persist: LikePersist) =>
-  (config?: Config) =>
-  <State>(store: Store<State>): Store<State> => {
-    persist({ store, ...config })
-    return store
+  (config?: Config): Curried => {
+    console.error(`/fp is deprecated, use regular form instead`)
+    return <State>(store: Store<State>): Store<State> => {
+      persist({ store, ...config })
+      return store
+    }
   }
