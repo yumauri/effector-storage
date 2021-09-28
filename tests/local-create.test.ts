4e51ca8ac27cba573ef6d1b629de3e6e1c0af9b1
yumauri <yumaa.verdin@gmail.com>
2021-09-28 12:43:02 +0300

    Add test for localStorage persist factory

---
+++ tests/local-create.test.ts
@@ -0,0 +1,59 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import { createStore } from 'effector'
+import { createStorageMock } from './mocks/storage.mock'
+import { createEventsMock } from './mocks/events.mock'
+import { create } from '../src/local'
+
+//
+// Mock `localStorage` and events
+//
+
+declare let global: any
+let events: ReturnType<typeof createEventsMock>
+
+test.before(() => {
+  global.localStorage = createStorageMock()
+  events = createEventsMock()
+  global.addEventListener = events.addEventListener
+})
+
+test.after(() => {
+  delete global.localStorage
+  delete global.addEventListener
+})
+
+//
+// Tests
+//
+
+test('key should be prefixed with keyPrefix', async () => {
+  const persist = create({
+    keyPrefix: 'app/',
+  })
+
+  const $counter = createStore(0, { name: 'counter' })
+  persist({ store: $counter })
+  assert.is($counter.getState(), 0)
+
+  //
+  ;($counter as any).setState(1)
+  assert.is(global.localStorage.getItem('counter'), null)
+  assert.is(global.localStorage.getItem('app/counter'), '1')
+
+  global.localStorage.setItem('app/counter', '2')
+  await events.dispatchEvent('storage', {
+    storageArea: global.localStorage,
+    key: 'app/counter',
+    oldValue: null,
+    newValue: '2',
+  })
+
+  assert.is($counter.getState(), 2)
+})
+
+//
+// Launch tests
+//
+
+test.run()
