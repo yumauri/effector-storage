b851d841efd0e504a99beb738725068e3fc1a504
yumauri <yumaa.verdin@gmail.com>
2022-02-13 02:44:08 +0300

    Run tests agains 4 different effector versions

---
+++ tests/effector-version.test.ts
@@ -0,0 +1,21 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import { version } from 'effector'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const effectorVersion: string = process.env.EFFECTOR_VERSION || '21.0.0'
+
+//
+// Tests
+//
+
+test('effector should be mocked', () => {
+  assert.is(version, effectorVersion)
+})
+
+//
+// Launch tests
+//
+
+test.run()
---
+++ tests/rewiremock.ts
@@ -0,0 +1,23 @@
+import rewiremock from 'rewiremock'
+
+/* eslint-disable camelcase */
+import * as effector_21_0_0 from 'effector-21-0-0'
+import * as effector_21_8_12 from 'effector-21-8-12'
+import * as effector_22_0_0 from 'effector-22-0-0'
+import * as effector_22_2_0 from 'effector-22-2-0'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const effectorVersion: string = process.env.EFFECTOR_VERSION || '21.0.0'
+
+rewiremock('effector').with(
+  {
+    '21.0.0': effector_21_0_0,
+    '21.8.12': effector_21_8_12,
+    '22.0.0': effector_22_0_0,
+    '22.2.0': effector_22_2_0,
+  }[effectorVersion]
+)
+
+rewiremock.enable()
+export { rewiremock }
