ff7e4558a5a983009a883949e3c762931c25d98f
yumauri <yumaa.verdin@gmail.com>
2023-01-24 21:45:33 +0300

    Update latest effector to 22.5.0

--- tests/rewiremock.ts
+++ tests/rewiremock.ts
@@ -4,7 +4,7 @@ import rewiremock from 'rewiremock'
 import * as effector_21_0_0 from 'effector-21-0-0'
 import * as effector_21_8_12 from 'effector-21-8-12'
 import * as effector_22_0_0 from 'effector-22-0-0'
-import * as effector_22_4_0 from 'effector-22-4-0'
+import * as effector_22_5_0 from 'effector-22-5-0'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -15,7 +15,7 @@ rewiremock('effector').with(
     '21.0.0': effector_21_0_0,
     '21.8.12': effector_21_8_12,
     '22.0.0': effector_22_0_0,
-    '22.4.0': effector_22_4_0,
+    '22.5.0': effector_22_5_0,
   }[effectorVersion]
 )
 
