a1a7e74125d85ebf209f27c075a7eb42b103ddc0
yumauri <yumaa.verdin@gmail.com>
2023-04-27 23:42:32 +0300

    Update dependencies

--- tests/rewiremock.ts
+++ tests/rewiremock.ts
@@ -2,7 +2,7 @@ import rewiremock from 'rewiremock'
 
 /* eslint-disable camelcase */
 import * as effector_22_0_0 from 'effector-22-0-0'
-import * as effector_22_8_1 from 'effector-22-8-1'
+import * as effector_22_8_2 from 'effector-22-8-2'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -11,7 +11,7 @@ const effectorVersion: string = process.env.EFFECTOR_VERSION || '22.0.0'
 rewiremock('effector').with(
   {
     '22.0.0': effector_22_0_0,
-    '22.8.1': effector_22_8_1,
+    '22.8.2': effector_22_8_2,
   }[effectorVersion]
 )
 
