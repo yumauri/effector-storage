8ae6493e49d94b6a77151b829eb044a899e7be58
yumauri <yumaa.verdin@gmail.com>
2023-07-16 00:43:37 +0300

    Don't run tests on mocked effector, use try workflow instead

--- tests/effector-version.test.ts
+++ tests/effector-version.test.ts
@@ -2,25 +2,19 @@ import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 import { version } from 'effector'
 
-const effectorVersion: string = process.env.EFFECTOR_VERSION || '22.0.0'
-const isTryEffectorVersion: string | undefined = process.env.TRY_VERSION
-const tryEffectorVersion: string | undefined = process.env.INPUT_EFFECTOR
+const tryDependency: string | undefined = process.env.INPUT_EFFECTOR
 
 //
 // Tests
 //
 
 test('effector should be mocked', () => {
-  if (isTryEffectorVersion) {
-    const tryVersion = tryEffectorVersion?.match(/(\d+\.\d+\.\d+)/)?.[1]
-    if (tryVersion) {
-      assert.is(version, tryVersion)
-    } else {
-      console.log('unknown try version:', tryEffectorVersion)
-      console.log('effector version:', version)
-    }
+  const tryVersion = tryDependency?.match(/(\d+\.\d+\.\d+)/)?.[1]
+  if (tryVersion) {
+    assert.is(version, tryVersion)
   } else {
-    assert.is(version, effectorVersion)
+    console.log('unknown try version:', tryDependency)
+    console.log('effector version:', version)
   }
 })
 
