8e18ec176e1e84ce92cac4ea9a4e7fb8a0e6cd0b
Igor Kamyshev <igor@kamyshev.me>
2024-08-06 19:44:11 +0700

    Edit size-limits

--- .size-limit.js
+++ .size-limit.js
@@ -3,7 +3,7 @@ module.exports = [
   {
     name: 'root persist, es module',
     path: 'build/index.js',
-    limit: '988 B',
+    limit: '989 B',
     import: '{ persist }',
     ignore: ['effector'],
     gzip: true,
@@ -11,7 +11,7 @@ module.exports = [
   {
     name: 'root persist, cjs module',
     path: 'build/index.cjs',
-    limit: '3863 B',
+    limit: '3869 B',
     // import: '{ persist }', // tree-shaking is not working with cjs
     ignore: ['effector'],
     gzip: true,
@@ -21,7 +21,7 @@ module.exports = [
   {
     name: 'core persist, es module',
     path: 'build/core/index.js',
-    limit: '983 B',
+    limit: '984 B',
     import: '{ persist }',
     ignore: ['effector'],
     gzip: true,
@@ -39,7 +39,7 @@ module.exports = [
   {
     name: 'tools, es module',
     path: 'build/tools/index.js',
-    limit: '289 B',
+    limit: '292 B',
     import: '{ async, either, farcached }',
     ignore: ['effector'],
     gzip: true,
@@ -47,7 +47,7 @@ module.exports = [
   {
     name: 'tools, cjs module',
     path: 'build/tools/index.cjs',
-    limit: '482 B',
+    limit: '485 B',
     // import: '{ async, either, farcached }', // tree-shaking is not working with cjs
     ignore: ['effector'],
     gzip: true,
@@ -65,7 +65,7 @@ module.exports = [
   {
     name: 'nil adapter, cjs module',
     path: 'build/nil/index.cjs',
-    limit: '139 B',
+    limit: '140 B',
     // import: '{ nil }', // tree-shaking is not working with cjs
     ignore: ['effector'],
     gzip: true,
@@ -75,7 +75,7 @@ module.exports = [
   {
     name: 'log adapter, es module',
     path: 'build/log/index.js',
-    limit: '139 B',
+    limit: '140 B',
     import: '{ log }',
     ignore: ['effector'],
     gzip: true,
@@ -93,7 +93,7 @@ module.exports = [
   {
     name: 'storage adapter, es module',
     path: 'build/storage/index.js',
-    limit: '399 B',
+    limit: '401 B',
     import: '{ storage }',
     ignore: ['effector'],
     gzip: true,
@@ -111,7 +111,7 @@ module.exports = [
   {
     name: '`localStorage` persist, es module',
     path: 'build/local/index.js',
-    limit: '1458 B',
+    limit: '1464 B',
     import: '{ persist }',
     ignore: ['effector'],
     gzip: true,
@@ -130,7 +130,7 @@ module.exports = [
   {
     name: 'core adapter, es module',
     path: 'build/index.js',
-    limit: '1438 B',
+    limit: '1444 B',
     import: '{ persist, local }',
     ignore: ['effector'],
     gzip: true,
@@ -138,7 +138,7 @@ module.exports = [
   {
     name: 'core adapter factory, es module',
     path: ['build/index.js', 'build/local/index.js'],
-    limit: '1440 B',
+    limit: '1446 B',
     import: {
       'build/index.js': '{ persist }',
       'build/local/index.js': '{ local }',
@@ -151,7 +151,7 @@ module.exports = [
   {
     name: '`sessionStorage` persist, es module',
     path: 'build/session/index.js',
-    limit: '1455 B',
+    limit: '1463 B',
     import: '{ persist }',
     ignore: ['effector'],
     gzip: true,
@@ -177,7 +177,7 @@ module.exports = [
   {
     name: 'query string persist, cjs module',
     path: 'build/query/index.cjs',
-    limit: '2292 B',
+    limit: '2295 B',
     // import: '{ persist }', // tree-shaking is not working with cjs
     ignore: ['effector'],
     gzip: true,
@@ -187,7 +187,7 @@ module.exports = [
   {
     name: 'memory adapter, es module',
     path: 'build/memory/index.js',
-    limit: '1076 B',
+    limit: '1078 B',
     import: '{ persist }',
     ignore: ['effector'],
     gzip: true,
@@ -205,7 +205,7 @@ module.exports = [
   {
     name: 'generic async storage adapter, es module',
     path: 'build/async-storage/index.js',
-    limit: '162 B',
+    limit: '165 B',
     import: '{ asyncStorage }',
     ignore: ['effector'],
     gzip: true,
@@ -223,7 +223,7 @@ module.exports = [
   {
     name: 'broadcast channel adapter, es module',
     path: 'build/broadcast/index.js',
-    limit: '370 B',
+    limit: '371 B',
     import: '{ broadcast }',
     ignore: ['effector'],
     gzip: true,
